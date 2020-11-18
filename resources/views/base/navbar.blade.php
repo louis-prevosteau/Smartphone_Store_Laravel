ed8fca6a18a04407febc9025e8164bcbfd61275d
louis-prevosteau <louis.prevosteau@laposte.net>
2020-11-18 08:00:37 +0100

    :fix: add nav-link

--- resources/views/base/navbar.blade.php
+++ resources/views/base/navbar.blade.php
@@ -4,7 +4,7 @@
             <li class="nav-item">
                 <a class="nav-link" href="{{route('smartphones.index')}}">Liste des produits</a>
             </li>
-            <li>
+            <li class="nav-item">
                 <a class="nav-link" href="{{route('smartphones.create')}}">Ajouter un produit</a>
             </li>
         </ul>
